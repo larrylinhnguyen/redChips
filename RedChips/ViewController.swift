a60c43f9012af58f7cc0766f875ce67bb7ea99af
larrylinhnguyen <Larry@larrys-macbook-pro.local>
2016-10-21 22:27:09 -0500

    Initial Commit

---
+++ RedChips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedChips
+//
+//  Created by Larry on 10/21/16.
+//  Copyright © 2016 Savings iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
